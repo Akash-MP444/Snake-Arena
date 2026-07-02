45287e2c287107f227fdea5b001f0b9113ff9b23
Akash MP <pes1ug24cs040@stu.pes.edu>
2026-07-02 15:08:00 +0530

    fix: serve Vite app with Express 5

--- server.cjs
+++ server.cjs
@@ -125,7 +125,7 @@ app.post("/api/leaderboard/clear-scores", async (_req, res) => {
 });
 app.use(express.static(path.join(__dirname, "dist")));
 
-app.get("*", (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
